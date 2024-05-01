0195608cfbd433e2e5d6cd10cd8066ae86b30863
elive7 <parksub020704@gmail.com>
2024-05-01 13:07:04 +0900

    [feat] post 생성 및 조회에 대한 에러/성공 메시지 정의

--- practice/src/main/java/opt/sopt/practice/common/GlobalExceptionHandler.java
+++ practice/src/main/java/opt/sopt/practice/common/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 import opt.sopt.practice.common.dto.ErrorMessage;
 import opt.sopt.practice.common.dto.ErrorResponse;
 import opt.sopt.practice.exception.NotFoundException;
+import opt.sopt.practice.exception.UnauthorizedActionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -22,6 +23,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     protected ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ErrorResponse.of(e.getErrorMessage()));
+    }
+
+    @ExceptionHandler(UnauthorizedActionException.class)
+    protected ResponseEntity<ErrorResponse> handleUnauthorizedActionException(UnauthorizedActionException e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
                 .body(ErrorResponse.of(e.getErrorMessage()));
     }
 
