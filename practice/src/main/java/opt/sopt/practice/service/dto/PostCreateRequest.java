2e329fef42895df646217e98b9191d6f297e7534
elive7 <parksub020704@gmail.com>
2024-05-01 16:16:27 +0900

    [fix] validation message 수정

--- practice/src/main/java/opt/sopt/practice/service/dto/PostCreateRequest.java
+++ practice/src/main/java/opt/sopt/practice/service/dto/PostCreateRequest.java
@@ -3,8 +3,8 @@ package opt.sopt.practice.service.dto;
 import jakarta.validation.constraints.Size;
 
 public record PostCreateRequest(
-        @Size(min = 1, max = 20, message = "게시글 제목이 최대 글자 수(10자)를 초과했습니다.")
+        @Size(min = 1, max = 20, message = "게시글 제목은 최소 1자 최대 20자여야 합니다.")
         String name,
-        @Size(min = 1, max = 200, message = "게시글 본문이 최대 글자 수(200자)를 초과했습니다.")
+        @Size(min = 1, max = 200, message = "게시글 내용은 최소 1자 최대 200자여야 합니다.")
         String content) {
 }
