4b2d7f4a234689001150218e7d114999753e3493
elive7 <parksub020704@gmail.com>
2024-06-02 15:15:00 +0900

    [fix] refreshToken 헤더로 담아서 요청 보내도록 수정

--- practice/src/main/java/opt/sopt/practice/auth/rds/controller/TokenController.java
+++ practice/src/main/java/opt/sopt/practice/auth/rds/controller/TokenController.java
@@ -8,6 +8,7 @@ import opt.sopt.practice.auth.rds.service.dto.RefreshTokenRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +20,7 @@ public class TokenController {
 
     @PostMapping("/token/refresh")
     public ResponseEntity<AccessTokenResponse> refreshAccessToken(
-            @RequestBody RefreshTokenRequest refreshTokenRequest) {
-        String refreshToken = refreshTokenRequest.refreshToken();
+            @RequestHeader String refreshToken) {
         return ResponseEntity.ok(tokenService.refreshAccessToken(refreshToken));
     }
 }
